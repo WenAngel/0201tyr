2a7833012f2a2778eb208765f2c1c1c93ead9d25
WenAngel <dada30227angel@yahoo.com.tw>
2019-02-01 16:50:23 +0800

    加入

---
+++ WinterVacation01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinterVacation01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int all=0;
+            int fir = 0;
+            int end = 1;
+         
+            for (all = 0; all <= 1836311903; all ++)
+            {
+
+                all = fir + end;
+                fir = end;
+                end = all;
+
+                Console.WriteLine(all);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WinterVacation04/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinterVacation04
+{
+    public partial class Form1 : Form
+    {
+        DateTime enter = new DateTime ();
+        public Form1()
+        {
+            InitializeComponent();
+            enter = DateTime.Now;
+            label1.Text = "進場時間: " + enter;
+        }
+    }
+}
